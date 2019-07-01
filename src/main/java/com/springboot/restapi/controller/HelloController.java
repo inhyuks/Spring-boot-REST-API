99f293b765350cdd0a15d591d9e1f69fb0591438
inhyuks <sih2341@naver.com>
2019-07-01 17:48:37 +0900

    swagger api 문서 관리 해보기

--- src/main/java/com/springboot/restapi/controller/HelloController.java
+++ src/main/java/com/springboot/restapi/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.springboot.restapi.controller;
 
 import com.springboot.restapi.domain.Hello;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"1. User"})
 @RestController
 @RequestMapping(value = "hello")
 public class HelloController {
+    @ApiOperation(value = "hello", notes = "hello")
     @GetMapping()
     public Hello get() {
         return new Hello(0, "Hello Spring Boot!");
